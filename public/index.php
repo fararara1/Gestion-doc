90894d93e0d6b79e81b2926db4f86e562e74203f
fararara1 <ettirfarah@gmail.com>
2026-07-23 16:40:10 +0100

    chore: ignore clever.json and clean up hosting configs

--- public/index.php
+++ public/index.php
@@ -2,28 +2,16 @@
 
 define('LARAVEL_START', microtime(true));
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-try {
-    $response = $kernel->handle(
-        $request = Illuminate\Http\Request::capture()
-    );
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-    $response->send();
+$response->send();
 
-    $kernel->terminate($request, $response);
-} catch (\Throwable $e) {
-    echo '<pre>';
-    echo 'ERROR: ' . $e->getMessage() . "\n";
-    echo $e->getTraceAsString() . "\n";
-    echo '</pre>';
-    exit(1);
-}
+$kernel->terminate($request, $response);
