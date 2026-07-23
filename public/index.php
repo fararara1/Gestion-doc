7cf31a7c05dfc33a19b79282d011b97cd77c8c21
fararara1 <ettirfarah@gmail.com>
2026-07-23 16:24:14 +0100

    chore: temporarily enable error display in public/index.php for Clever Cloud debug

--- public/index.php
+++ public/index.php
@@ -2,16 +2,28 @@
 
 define('LARAVEL_START', microtime(true));
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+try {
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
 
-$response->send();
+    $response->send();
 
-$kernel->terminate($request, $response);
+    $kernel->terminate($request, $response);
+} catch (\Throwable $e) {
+    echo '<pre>';
+    echo 'ERROR: ' . $e->getMessage() . "\n";
+    echo $e->getTraceAsString() . "\n";
+    echo '</pre>';
+    exit(1);
+}
